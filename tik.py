f2bc04d341b7c55741ace0b88972d377b69b98c2
jagsblast <33574320+jagsblast@users.noreply.github.com>
2020-09-21 22:27:33 +0100

    Add files via upload

--- tik.py
+++ tik.py
@@ -1,11 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+new = 0
+current = 0
 while True:
     URL = 'http://localhost:80'
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, 'html.parser')
     follower = soup.body.text
-    print(follower)
+    new = int(follower)
+    if new > current:
+        print (new, "you gained a new follower")
+        current = new
+    else:
+        print(current, "get in the bin m8")
     time.sleep(5)
